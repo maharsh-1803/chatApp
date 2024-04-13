30a349d0870f0d35441f880c2cdf8aae64cea68b
maharsh-1803 <maharshpatel1803@gmail.com>
2024-04-14 02:04:57 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotevn from 'dotenv'
+import cookieParser from 'cookie-parser'
+import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotevn.config();
+
+app.use(express.json());
+app.use(cookieParser())
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use('/api/auth',authRoutes);
+app.use('/api/message',messageRoutes)
+app.use('/api/users',userRoutes)
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log(`Server is Running on port ${PORT}`)
+})
