f091dcd98008303d4dd53a77b44b6c6c0c3d8e0a
maharsh-1803 <maharshpatel1803@gmail.com>
2024-04-15 22:44:02 +0530

    get message done

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -2,7 +2,7 @@ import {create} from 'zustand'
 
 const useConversation = create((set)=>({
     selectedConversation:null,
-    setSelectedConvesation: (selectedConversation)=> set({selectedConversation}),
+    setSelectedConversation: (selectedConversation)=> set({selectedConversation}),
     messages:[],
     setMessages: (messages) => set({messages}),
 }));
