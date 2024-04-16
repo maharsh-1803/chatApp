7c0fc77f9e27a0e5fdce1747665a4dfec7e1ce14
maharsh-1803 <maharshpatel1803@gmail.com>
2024-04-16 22:51:38 +0530

    small change

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,11 @@ app.use('/api/users',userRoutes)
 
 app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
 server.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`Server is Running on port ${PORT}`)
